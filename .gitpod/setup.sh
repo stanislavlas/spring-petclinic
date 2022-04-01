ebee4e361dde6cbcdbc4b1d981ff3bc499f1103b
Administrator <admin@example.com>
2022-04-01 15:54:02 +0000

    sudo

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-echo "start setup"
-echo $GITPOD_GIT_USER_NAME
-echo $GITPOD_GIT_USER_EMAIL
-
-sudo touch /home/gitpod/.gitconfig
-sed "s+<user.name>+${GITPOD_GIT_USER_NAME}+g; s+<user.email>+${GITPOD_GIT_USER_EMAIL}+g" ".gitpod/.gitconfig" > /home/gitpod/.gitconfig
+sed "s+<user.name>+$1+g; s+<user.email>+$2+g" ".gitpod/.gitconfig" > /home/gitpod/.gitconfig
 
 git clone https://github.com/AGWA/git-crypt.git
 cd git-crypt
-sudo make install
+make install
 cd ..
 rm -rf git-crypt
