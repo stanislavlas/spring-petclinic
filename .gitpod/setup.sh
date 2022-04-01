0cdeadcd5c5d2018040cccd0c293c993196bd474
Administrator <admin@example.com>
2022-04-01 15:15:02 +0000

    asdas

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed "s+<user.name>+${GITPOD_GIT_USER_NAME}+g; s+<user.email>+${GITPOD_GIT_USER_EMAIL}+g" ".gitconfig" > /home/gitpod/.gitconfig
+sed "s+<user.name>+${GITPOD_GIT_USER_NAME}+g; s+<user.email>+${GITPOD_GIT_USER_EMAIL}+g" ".gitpod/.gitconfig" > /home/gitpod/.gitconfig
 
 git clone https://github.com/AGWA/git-crypt.git
 cd git-crypt
