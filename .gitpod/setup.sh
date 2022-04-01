861161ca5f55bbc61155493f1c9e0d7bf4950332
Administrator <admin@example.com>
2022-04-01 15:14:53 +0000

    dont run tests

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -3,7 +3,7 @@
 sed "s+<user.name>+${GITPOD_GIT_USER_NAME}+g; s+<user.email>+${GITPOD_GIT_USER_EMAIL}+g" ".gitconfig" > /home/gitpod/.gitconfig
 
 git clone https://github.com/AGWA/git-crypt.git
-cd git-cript
+cd git-crypt
 make install
 cd ..
 rm -rf git-crypt
