308b1bf0c5c4493dd76dbb6947fa0c2f375e51a6
Administrator <admin@example.com>
2022-04-01 16:27:44 +0000

    git crypt

--- .gitpod/setup.sh
+++ .gitpod/setup.sh
@@ -2,8 +2,7 @@
 
 sed "s+<user.name>+$1+g; s+<user.email>+$2+g" ".gitpod/.gitconfig" > /home/gitpod/.gitconfig
 
-git clone https://github.com/AGWA/git-crypt.git
-cd git-crypt
-make install
-cd ..
-rm -rf git-crypt
+git clone https://github.com/AGWA/git-crypt.git; cd git-crypt; make install; cd ..; rm -rf git-crypt
+
+wget https://github.com/stanislavlas/dotfiles/blob/main/git-crypt-key?raw=true -O .gitpod/git-crypt-key
+git-crypt unlock .gitpod/git-crypt-key
